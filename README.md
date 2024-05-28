e782898817c7d5d8a9d6c8f15db8eed0f4459311
Adriana Delgado <169312599+adi-0402@users.noreply.github.com>
2024-05-28 18:29:42 -0500

    initial command

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vehicles
+Esta aplicacion sirve para visualizar los datos de las ventas de vehiculos. 
+Contiene graficos interactivos para una mejor visualizacion y comprension.
---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+
+st.header('Vehicles')  # crear un encabezado
+# crear una casilla de verificacion
+hist_build = st.checkbox('Construir histograma')
+
+if hist_build:  # si la casilla de verificación está seleccionada
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+# crear una casilla de verificacion
+disp_build = st.checkbox('Construir grafico de dispersion')
+
+if disp_build:  # si la casilla de verificación está seleccionada
+    # escribir un mensaje
+    st.write(
+        'Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un grafico de dispersion
+    fig = px.scatter(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
